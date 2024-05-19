9659a463bbe73dfa1629ac6769ced2f7c83c1890
asadmukhtarr <axad03213@gmail.com>
2024-05-19 20:41:25 +0500

    views

--- home.php
+++ home.php
@@ -6,6 +6,14 @@
     include('actions/cn.php');
     $query = "SELECT * FROM `users`";
     $result = mysqli_query($cn,$query) or die('cant run query');
+    $qry = "SELECT * FROM `views`";
+    $res = mysqli_query($cn,$qry) or die('cant run query for views count');
+    $n = mysqli_fetch_array($res);
+    $n = $n['views'];
+    $n = $n + 1;
+    $q = "UPDATE `views` SET views='$n' WHERE id='1'";
+    mysqli_query($cn,$q) or die('cant run query for add views data');
+
 ?>
     <div class="container mt-4">
         <div class="row">
@@ -16,6 +24,7 @@
                 <div class="card">
                     <div class="card-header">
                         Welcome <?php echo $_SESSION['name']; ?>
+                        <span class="badge badge-danger float-right">Views: <?php echo $n; ?></span>
                     </div>
                     <div>
                         <table class="table table-bordered table-striped table-hover">
